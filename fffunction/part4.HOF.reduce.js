10d3cfdf1b3fc2ef8bf1b9ad17fce705dc3ec053
Timothy Merritt <timmybytes@gmail.com>
2021-01-07 09:55:00 -0800

    (feat): Test git status on UI colors

--- fffunction/part4.HOF.reduce.js
+++ fffunction/part4.HOF.reduce.js
@@ -1,2 +1,23 @@
 /* Adv. Reduce */
 // ---------------------------
+import fs from 'fs';
+
+const output = fs
+  .readFileSync('part4.data.txt', 'utf8')
+  .trim()
+  .split('\n')
+  .map(line => line.split(','))
+  .reduce((customers, line) => {
+    customers[line[0]] = [];
+    customers[line[0]].push({ name: line[1], price: line[2], quantity: line[3] });
+    return customers;
+  }, {});
+
+console.log('output', JSON.stringify(output, null, 2));
+
+/*
+border: #a7c080
+background: #2a2426
+
+Activity Bar
+border: #323d43 */

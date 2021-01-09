3ae5e2654e377bee9009726405c210141f931bad
Timothy Merritt <timmybytes@gmail.com>
2021-01-09 10:49:58 -0800

    (feat): Add solution to reverseInteger

--- src/reverseInteger.js
+++ src/reverseInteger.js
@@ -23,15 +23,31 @@ Example 4:
   Output: 0
 */
 
+/*
 const reverse = x => {
-  if (-(2 ** 31) <= x && x <= 2 ** 31 - 1) {
+  console.log(x < 2147483647);
+  if (Math.abs(x) > -2147483648 && Math.abs(x) < 2147483647) {
     let rev = x.toString().split('');
     if (rev[0] === '-') {
       rev.shift();
       rev.reverse().unshift('-');
+      console.log(rev.reverse().unshift('-'));
       return rev.join('');
     }
+    console.log(rev.reverse().join(''));
     return rev.reverse().join('');
   }
+  console.log(0);
   return 0;
 };
+reverse(1534236469); // 9646324351
+*/
+
+// Solution from Discussion
+var reverse = function (x) {
+  const reversed = parseInt(Math.abs(x).toString().split('').reverse().join('')) * Math.sign(x);
+  console.log(reversed <= 0x7fffffff && reversed >= -0x80000000 ? reversed : 0);
+  return reversed <= 0x7fffffff && reversed >= -0x80000000 ? reversed : 0;
+};
+
+reverse(1534236469); // 0

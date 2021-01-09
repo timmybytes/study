eee5a45e65fc58480bd980719424b1e6e03a0ea2
Timothy Merritt <timmybytes@gmail.com>
2021-01-09 10:20:45 -0800

    (feat): Add reverseInteger

---
+++ src/reverseInteger.js
@@ -0,0 +1,37 @@
+/*
+Leetcode #7 - Reverse Integer - Easy
+------------------------------------------------------------------------
+Given a 32-bit signed integer, reverse digits of an integer.
+
+Note:
+Assume we are dealing with an environment that could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+
+Example 1:
+  Input: x = 123
+  Output: 321
+
+Example 2:
+  Input: x = -123
+  Output: -321
+
+Example 3:
+  Input: x = 120
+  Output: 21
+
+Example 4:
+  Input: x = 0
+  Output: 0
+*/
+
+const reverse = x => {
+  if (-(2 ** 31) <= x && x <= 2 ** 31 - 1) {
+    let rev = x.toString().split('');
+    if (rev[0] === '-') {
+      rev.shift();
+      rev.reverse().unshift('-');
+      return rev.join('');
+    }
+    return rev.reverse().join('');
+  }
+  return 0;
+};

97c12303de90b300fb642f535b625be4246dae79
Timothy Merritt <timmybytes@gmail.com>
2021-05-22 21:08:53 -0700

    chore: Revise boolToWord for O(1)

--- JS-CS/boolToWord.js
+++ JS-CS/boolToWord.js
@@ -1,6 +1,18 @@
 function boolToWord(bool) {
-  if (bool) {
-    return 'yes';
-  }
-  return 'no';
+  // if (bool) {
+  //   return 'yes';
+  // }
+  // return 'no';
+
+  return bool ? 'yes' : 'no';
 }
+
+console.log(boolToWord(true));
+console.log(boolToWord(false));
+console.log(boolToWord(0));
+console.log(boolToWord(1));
+console.log(boolToWord(''));
+console.log(boolToWord('a'));
+console.log(boolToWord(null));
+console.log(boolToWord(undefined));
+console.log(boolToWord({}));
